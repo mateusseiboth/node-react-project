7cb529d60ab2c961d8c6e19fc6ca1e0d1f816032
mateusseiboth <mateusseiboth@gmail.com>
2022-12-28 10:57:07 -0400

    Criado componente de falta de acesso

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,41 @@
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Box,
+} from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const Post = () => {
+
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    axios.get(" /api/v1/login/").then(function(response){
+      console.log(response.data.user[0])
+      setUsername(response.data.user[0].username)
+  })
+
+}, [])
+
+
+  //cria a página
+  return (
+    <Box flex={10} p={{ xs: 0, md: 11 }} minHeight="900px">
+    <Card sx={{ margin: 1 }} >
+      <CardContent>
+      <Typography color="danger" align="center" variant="h3" gutterBottom>
+        Você não tem acesso a esta página, por favor não insista
+      </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+      </CardActions>
+    </Card>
+    </Box>
+  );
+
+};
+
+export default Post;
