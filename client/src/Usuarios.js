a181710dfa059e68db1b511454f6c883f17c4729
mateusseiboth <mateusseiboth@gmail.com>
2022-12-28 08:58:52 -0400

    Adicionada verificação porca de login por modulo

--- client/src/Usuarios.js
+++ client/src/Usuarios.js
@@ -2,9 +2,22 @@ import Sidebar from "./components/Sidebar";
 import Feed from "./components/FeedUser";
 import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 function App() {
+  useEffect(() => {
+    axios.get("/api/v1/login/").then((response) => {
+      if (response.data.loggedIn === false) {
+        window.location = "/login";
+      } else {
+  
+      }
+    });
+  }
+  , []);
   const [mode, setMode] = useState("dark");
 
   const darkTheme = createTheme({
