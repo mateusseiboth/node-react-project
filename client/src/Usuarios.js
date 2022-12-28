95a8bcd4631dd850a41b21d9410706c92d6e7230
mateusseiboth <mateusseiboth@gmail.com>
2022-12-28 10:56:27 -0400

    Adição de verificação de nivel

--- client/src/Usuarios.js
+++ client/src/Usuarios.js
@@ -2,22 +2,33 @@ import Sidebar from "./components/Sidebar";
 import Feed from "./components/FeedUser";
 import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
+import Error from './components/Error';
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
 function App() {
+
+  const [nivel, setNivel] = useState(true);
+  
   useEffect(() => {
     axios.get("/api/v1/login/").then((response) => {
       if (response.data.loggedIn === false) {
         window.location = "/login";
-      } else {
-  
+      } else 
+      if (response.data.user[0].nivel === 1){
+        setNivel(false)
+        console.log(nivel)
+      }
+      else {
+        setNivel(true)
+        console.log(nivel)
       }
     });
   }
   , []);
+
   const [mode, setMode] = useState("dark");
 
   const darkTheme = createTheme({
@@ -32,7 +43,14 @@ function App() {
         <Navbar />
         <Stack direction="row" spacing={2} justifyContent="space-between">
         <Sidebar setMode={setMode} mode={mode}/>
+
+        {nivel ? (
+          <Error />
+        ) : (
+          
           <Feed />
+        )}
+
         </Stack>
       </Box>
     </ThemeProvider>
