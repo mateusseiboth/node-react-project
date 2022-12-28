d99802eecc4aa6bd27c652b9dd4394bf883cfd4f
mateusseiboth <mateusseiboth@gmail.com>
2022-12-28 10:56:14 -0400

    Remoção de libs não usadas

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,8 @@ import Tipos from "./Tipo.js";
 import FeedIndex from "./inicio.js";
 import Login from "./Login.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import axios from "axios";
-function App() {
 
-  axios.defaults.withCredentials = true;
+function App() {
 
   return (
       <Router>
