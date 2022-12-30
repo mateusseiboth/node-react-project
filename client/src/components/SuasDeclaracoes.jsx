94a748edac35af319df16a0b314ccc846450be22
mateusseiboth <mateusseiboth@gmail.com>
2022-12-30 13:11:17 -0400

    Removidas importações não usadas

--- client/src/components/SuasDeclaracoes.jsx
+++ client/src/components/SuasDeclaracoes.jsx
@@ -1,44 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { styled } from '@mui/material/styles';
-
 import {
-  Paper,
   Card,
-  CardActions,
   CardContent,
   CardHeader,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  tableCellClasses,
   Typography,
   Grid,
   Box,
-
 } from "@mui/material";
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
 const Post = () => {
   const [username, setUsername] = useState('');
   const [userId, setUserId] = useState('');
